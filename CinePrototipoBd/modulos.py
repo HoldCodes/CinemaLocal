32c2a279544c3a0817d819e9c31fc5fdc5633806
Kaio Pedro de Souza <82689646+KaioPedro1@users.noreply.github.com>
2022-05-08 20:07:09 -0300

    Delete CinePrototipoBd

--- CinePrototipoBd/modulos.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-from tkinter import *
-from tkinter import messagebox, ttk
-import sqlite3
-from PIL import ImageTk, Image
-from loginGui import funcDb
-from io import BytesIO
-import os , sys
-import tempfile
