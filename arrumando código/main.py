924c07c50ce10ace9cf16c313fc23b1c0f49400b
Kaio Pedro de Souza <82689646+KaioPedro1@users.noreply.github.com>
2022-06-14 22:06:13 -0300

    Add files via upload

---
+++ arrumando código/main.py
@@ -0,0 +1,10 @@
+import janelas
+import loginGui
+from telaAdministrador import adminGui
+from modulos import *
+
+def main():
+    #loginGui.LoginScreen()
+    adminGui.adminGui()
+if __name__ == '__main__':
+    main()
